874c75f5e20d2a9e3f4dd56986269cd4664f9150
Drakunos0 <alessandropedrozo@gmail.com>
2020-10-26 12:55:39 -0300

    ajuste static

--- fusion/settings.py
+++ fusion/settings.py
@@ -134,8 +134,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = 'media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Email teste console
