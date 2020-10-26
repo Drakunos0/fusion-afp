aa4fd2b0c2b36fc35baf9955df0d4356acd48fd3
Drakunos0 <alessandropedrozo@gmail.com>
2020-10-26 12:02:52 -0300

    ajuste static

--- fusion/settings.py
+++ fusion/settings.py
@@ -133,11 +133,11 @@ USE_TZ = True
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'static'))
-STATICFILES_DIRS = [
+STATICFILES_DIRS = (
     os.path.normpath(os.path.join(BASE_DIR, 'static')),
-]
+)
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # STATICFILES_DIRS = (
