0c43a0ca936c23ab8caba3a096d99cdf0d1378a8
Drakunos0 <alessandropedrozo@gmail.com>
2020-12-11 20:22:41 -0300

    django

--- fusion/settings.py
+++ fusion/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import dj_database_url
-import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -28,9 +28,6 @@ SECRET_KEY = 'ky&pmgwpz%pp^a%hoj0*&4vaesm^_11utvz#8=+p&(-me=5s%3'
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
 
 
 # Application definition
